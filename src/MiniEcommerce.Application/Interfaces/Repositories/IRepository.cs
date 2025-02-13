dbc44cbe3d82f62a241ee179a7c8c5368b76d8fc
randomuser <46854586+bayrakhasancan@users.noreply.github.com>
2025-02-13 12:48:35 +0300

    feat: add base repository interfaces, BaseEntity and Value Object

---
+++ src/MiniEcommerce.Application/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniEcommerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEcommerce.Application.Interfaces.Repositories
+{
+    public interface IRepository<T> where T : BaseAuditableEntity
+    {
+        DbSet<T> Table { get; }
+    }
+}
---
+++ src/MiniEcommerce.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEcommerce.Domain.Common
+{
+    public abstract class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+        public string? CreatedBy { get; init; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+    }
+}
